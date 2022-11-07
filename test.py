180f6d22bb285dca9435d5d0842d75cf86dfe7ce
Michael Moore <michaelrmoore26@gmail.com>
2022-11-07 15:38:35 -0500

    gcs

--- test.py
+++ test.py
@@ -7,6 +7,8 @@ from apache_beam.io import ReadFromText
 from apache_beam.io import WriteToText
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.options.pipeline_options import SetupOptions
+from apache_beam.dataframe.convert import to_dataframe
+
 import findspark
 findspark.init()
 from pyspark.sql import SparkSession
@@ -16,28 +18,68 @@ filename ='/Users/mikemoore26/Downloads/archive (36)/yelp_academic_dataset_tip.j
 #df = spark.read.json(filename)
 
 
+class Json_Csv(beam.DoFn):
+  def json_csv(self,line: str) -> beam.pvalue.PCollection:
+    json_csv(line)
 
 def json_csv(line : str) -> beam.pvalue.PCollection:
   import json
   import csv
   import pandas as pd
-
   line = json.loads(line)
-  series = pd.Series(line)
+  series = [pd.Series(line)]
+
+  text = ''
+  for i in range(len(series)):
+    text += str(series[i]).strip()
+    if i != len(series):
+      text += ':'
+
+  return lines
 
 
-
-  return series
-
-def run():
+def run(argv=None, save_main_session=True):
+  """Main entry point; defines and runs the wordcount pipeline."""
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+      '--input',
+      dest='input',
+      default='gs://yelp_bucket-mm',
+      help='Input file to process.')
+  parser.add_argument(
+      '--output',
+      dest='output',
+      required=True,
+      help='Output file to write results to.')
+  known_args, pipeline_args = parser.parse_known_args(argv)
 
   with beam.Pipeline() as pipeline:
   # Options
     print(type(pipeline))
     lines = pipeline | 'reading' >> beam.io.ReadFromText(filename) \
+            | 'convert method' >> (beam.ParDo(Json_Csv.json_csv()).with_output_types(str)) \
+            | beam.Map(print)
+
+  def format_result(line):
+
+    return [x + ':' for x in line]
+
+  output = lines | 'Format' >> beam.MapTuple(format_result)
+
+  output | 'Write' >> WriteToText(known_args.output)
+
+    # print(lines)
+
+def run_test():
+  with beam.Pipeline() as pipeline:
+    # Options
+    print(type(pipeline))
+    lines = pipeline | 'reading' >> beam.io.ReadFromText(filename) \
             | 'convert method' >> beam.Map(json_csv) \
             | beam.Map(print)
 
+    # df = to_dataframe(lines)
+
     # print(lines)
 if __name__ == '__main__':
-  run()
+  run_test()
